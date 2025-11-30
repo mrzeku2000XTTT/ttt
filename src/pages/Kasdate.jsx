6debb6d92e34898810af442fcfaa907d9040be71
Mr Zeku <mrzeku12@gmail.com>
2025-11-30 14:44:18 +0000

    File changes

--- src/pages/Kasdate.jsx
+++ src/pages/Kasdate.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 export default function KasdatePage() {
   return (
-    <div className="fixed inset-0 w-full h-full bg-black">
+    <div className="fixed inset-0 w-full h-full bg-black" style={{ top: 0, left: 0, right: 0, bottom: 0 }}>
       <iframe
         src="https://krypton-connect-2b48661d.base44.app"
         className="w-full h-full border-0"
         title="Kasdate App"
-        allow="clipboard-read; clipboard-write"
+        allow="clipboard-read; clipboard-write; camera; microphone; geolocation"
+        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
       />
     </div>
   );
--- src/pages/Kolade.jsx
+++ src/pages/Kolade.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 export default function KoladePage() {
   return (
-    <div className="fixed inset-0 w-full h-full bg-black">
+    <div className="fixed inset-0 w-full h-full bg-black" style={{ top: 0, left: 0, right: 0, bottom: 0 }}>
       <iframe
         src="https://arch-book-a1d75318.base44.app/"
         className="w-full h-full border-0"
         title="Kolade App"
-        allow="clipboard-read; clipboard-write"
+        allow="clipboard-read; clipboard-write; camera; microphone; geolocation"
+        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
       />
     </div>
   );
