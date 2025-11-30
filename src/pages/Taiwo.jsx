58e69173cffdf526a89d6dfbc214dea8609a96f3
Mr Zeku <mrzeku12@gmail.com>
2025-11-30 00:08:44 +0000

    File changes

--- src/pages/Taiwo.jsx
+++ src/pages/Taiwo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function TaiwoPage() {
   return (
-    <div className="fixed inset-0 bg-black">
+    <div className="fixed inset-0 bg-black z-40" style={{ top: 'calc(var(--sat, 0px) + 7.5rem)', bottom: 0 }}>
       <iframe
         src="https://kasware-quest-69d8b5d2.base44.app/"
         className="w-full h-full border-0"
