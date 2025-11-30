b7395c051c1030bed4814715297703154008531b
Mr Zeku <mrzeku12@gmail.com>
2025-11-30 16:53:59 +0000

    File changes

--- src/pages/KASari.jsx
+++ src/pages/KASari.jsx
@@ -2,12 +2,11 @@ import React from "react";
 
 export default function KASariPage() {
   return (
-    <div className="fixed bg-black" style={{ 
+    <div className="fixed bg-black lg:ml-12" style={{ 
       top: 'calc(7.5rem + var(--sat, 0px))',
       left: 0,
       right: 0,
-      bottom: 0,
-      paddingBottom: 'calc(env(safe-area-inset-bottom, 0px) + 4rem)'
+      bottom: 'calc(4rem + env(safe-area-inset-bottom, 0px))'
     }}>
       <iframe
         src="https://kaspa-emergency-response-097a9b19.base44.app"
