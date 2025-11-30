6f18b892c88f22257cc0f47ebf5f0292d03218d4
Mr Zeku <mrzeku12@gmail.com>
2025-11-30 14:49:38 +0000

    File changes

--- src/pages/Kasdate.jsx
+++ src/pages/Kasdate.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function KasdatePage() {
   return (
-    <div className="fixed bg-black" style={{ top: 'calc(8rem + var(--sat, 0px))', left: 0, right: 0, bottom: 0 }}>
+    <div className="fixed bg-black lg:ml-12" style={{ top: 'calc(8rem + var(--sat, 0px))', left: 0, right: 0, bottom: 0 }}>
       <iframe
         src="https://krypton-connect-2b48661d.base44.app"
         className="w-full h-full border-0"
--- src/pages/Kolade.jsx
+++ src/pages/Kolade.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function KoladePage() {
   return (
-    <div className="fixed bg-black" style={{ top: 'calc(8rem + var(--sat, 0px))', left: 0, right: 0, bottom: 0 }}>
+    <div className="fixed bg-black lg:ml-12" style={{ top: 'calc(8rem + var(--sat, 0px))', left: 0, right: 0, bottom: 0 }}>
       <iframe
         src="https://arch-book-a1d75318.base44.app/"
         className="w-full h-full border-0"
