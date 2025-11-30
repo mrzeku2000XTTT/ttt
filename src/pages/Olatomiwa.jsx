e8671212371f3278918e107ea2173822cbc0868c
Mr Zeku <mrzeku12@gmail.com>
2025-11-30 07:21:42 +0000

    File changes

--- src/pages/Olatomiwa.jsx
+++ src/pages/Olatomiwa.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function OlatomiwaPage() {
   return (
-    <div className="fixed inset-0 bg-black" style={{ paddingTop: 'calc(var(--sat, 0px) + 7.5rem)' }}>
+    <div className="fixed inset-0 bg-black z-40" style={{ top: 'calc(var(--sat, 0px) + 7.5rem)', bottom: 0 }}>
       <iframe
-        src="https://kas-connect-ca83bd9d.base44.app/"
+        src="https://kaswear-fashion-89db4060.base44.app"
         className="w-full h-full border-0"
         title="Olatomiwa"
         allow="clipboard-read; clipboard-write"
