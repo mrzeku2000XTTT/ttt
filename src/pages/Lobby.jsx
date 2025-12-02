3af230a622ecb67ab8121f634fc58c6ccf6c222c
Tanner Geronimo <yvsie21@gmail.com>
2025-12-02 03:42:32 +0000

    File changes

--- src/pages/Lobby.jsx
+++ src/pages/Lobby.jsx
@@ -30,7 +30,7 @@ export default function LobbyPage() {
           <img
             src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6901295fa9bcfaa0f5ba2c2a/e91d0d293_image.png"
             alt="Cosmic Portal"
-            className="w-full h-full object-cover scale-[12]"
+            className="w-full h-full object-cover scale-[10]"
           />
         </motion.div>
 
