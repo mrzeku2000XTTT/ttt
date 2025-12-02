e6119a7a0ab546a429476d95d1fd8eea9a4172b7
Tanner Geronimo <yvsie21@gmail.com>
2025-12-02 14:57:48 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -74,6 +74,7 @@ export default function HomePage() {
 
         {/* Dark Gradient Overlay */}
         <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/40 to-black/60" style={{ zIndex: 2 }} />
+      </div>
       
       {/* Logout Button */}
       {user && (
