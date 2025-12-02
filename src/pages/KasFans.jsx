c6caa0f9794027280395175e79e6d74c1b333a13
Tanner Geronimo <yvsie21@gmail.com>
2025-12-02 19:34:43 +0000

    File changes

--- src/pages/KasFans.jsx
+++ src/pages/KasFans.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function KasFansPage() {
   return (
-    <div className="w-full h-screen bg-black" style={{ paddingTop: 'calc(var(--sat, 0px) + 8rem)' }}>
+    <div className="fixed inset-0 w-full h-full" style={{ top: 'calc(var(--sat, 0px) + 7.5rem)' }}>
       <iframe
         src="https://kaspa-fans-hub-427b38c4.base44.app/dashboard"
         className="w-full h-full border-0"
