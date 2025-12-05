260e5b5a523b4211460d533afff1035747909920
Zariyah Montefalco <zariyahmontefalco28@gmail.com>
2025-12-05 10:33:38 +0000

    File changes

--- src/pages/AYOMUIZ2.jsx
+++ src/pages/AYOMUIZ2.jsx
@@ -2,26 +2,10 @@ import React from "react";
 
 export default function AYOMUIZ2Page() {
   return (
-    <div
-      style={{
-        position: "fixed",
-        top: "calc(var(--sat, 0px) + 7.5rem)",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        width: "100%",
-        height: "auto",
-        overflow: "hidden",
-      }}
-    >
+    <div className="fixed inset-0" style={{ top: "calc(var(--sat, 0px) + 7.5rem)" }}>
       <iframe
         src="https://kaspa-ng-62ab4fc0.base44.app"
-        style={{
-          width: "100%",
-          height: "100%",
-          border: "none",
-          display: "block",
-        }}
+        className="w-full h-full border-0"
         title="Ayomuiz2"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
