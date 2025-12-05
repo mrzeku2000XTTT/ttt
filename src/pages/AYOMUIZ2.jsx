9d25a439abe653043c6de0d16e05a23273d577c3
Zariyah Montefalco <zariyahmontefalco28@gmail.com>
2025-12-05 10:35:20 +0000

    File changes

--- src/pages/AYOMUIZ2.jsx
+++ src/pages/AYOMUIZ2.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function AYOMUIZ2Page() {
   return (
-    <div className="fixed inset-0" style={{ top: "calc(var(--sat, 0px) + 7.5rem)" }}>
+    <div className="fixed inset-0 lg:left-12" style={{ top: "calc(var(--sat, 0px) + 7.5rem)" }}>
       <iframe
         src="https://kaspa-ng-62ab4fc0.base44.app"
         className="w-full h-full border-0"
