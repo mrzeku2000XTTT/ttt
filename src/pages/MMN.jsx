2c200339224f3364f4821792b460d14db615557d
Zariyah Montefalco <zariyahmontefalco28@gmail.com>
2025-12-06 16:48:30 +0000

    File changes

--- src/pages/MMN.jsx
+++ src/pages/MMN.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function MMNPage() {
   return (
-    <div className="fixed inset-0 bg-black" style={{ 
+    <div className="fixed bg-black lg:ml-12" style={{ 
       top: 'calc(var(--sat, 0px) + 7.5rem)',
-      bottom: 0,
+      bottom: 'calc(var(--sab, 0px) + 4rem)',
       left: 0,
       right: 0
     }}>
