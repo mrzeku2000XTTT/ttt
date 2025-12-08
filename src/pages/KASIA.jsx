9c7dd0b5ab4e44fa5eafe443405528258fda2e4d
Zariyah Montefalco <zariyahmontefalco28@gmail.com>
2025-12-08 12:21:04 +0000

    File changes

--- src/pages/KASIA.jsx
+++ src/pages/KASIA.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function KASIAPage() {
   return (
     <div className="w-full min-h-screen bg-black flex flex-col">
-      <div className="flex-1 w-full max-w-[2000px] mx-auto pt-6">
+      <div className="flex-1 w-full max-w-[2000px] mx-auto pt-3">
         <iframe
           src="https://kasia.fyi"
-          className="w-full h-[calc(100vh-8rem)] border-0 rounded-lg"
+          className="w-full h-[calc(100vh-8.5rem)] border-0"
           title="KASIA"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
