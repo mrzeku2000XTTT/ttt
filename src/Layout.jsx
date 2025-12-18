2fdf5f6eab373213a99afe5eea87bd3ccecd5d2e
Zekk <zekubusiness24@gmail.com>
2025-12-18 23:49:51 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -203,8 +203,9 @@ export default function Layout({ children, currentPageName }) {
   const isTheRealmPage = currentPageName === "TheRealm";
   const isTruthLandingPage = currentPageName === "TruthLanding";
   const isUNIPage = currentPageName === "UNI";
+  const isShiLLzPage = currentPageName === "ShiLLz";
 
-  if (isHomePage || isLobbyPage || isEarthPage || isCountryDetailPage || isKonektPage || isArea51Page || isTheRealmPage || isTruthLandingPage || isUNIPage) {
+  if (isHomePage || isLobbyPage || isEarthPage || isCountryDetailPage || isKonektPage || isArea51Page || isTheRealmPage || isTruthLandingPage || isUNIPage || isShiLLzPage) {
     return (
       <React.Suspense fallback={<div className="min-h-screen bg-black" />}>
         <VideoPlayerProvider>
