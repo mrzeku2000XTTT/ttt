3c09390f81ac5d8b211c5631412586a3275f692d
Mr Zeku <mrzeku12@gmail.com>
2025-12-28 17:28:33 +0000

    Ksocial page with iframe

--- src/pages/AppStore.jsx
+++ src/pages/AppStore.jsx
@@ -70,7 +70,7 @@ export default function AppStorePage() {
     { name: "Kurncy", icon: "Link2", path: "Kurncy", category: "Finance", defaultIcon: "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6901295fa9bcfaa0f5ba2c2a/009f28f08_image.png" },
     { name: "K gigZ", icon: "Link2", path: "KGigZ", category: "Tools", defaultIcon: "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6901295fa9bcfaa0f5ba2c2a/6ff6d06b2_image.png" },
     { name: "Poki", icon: "Link2", path: "Poki", category: "Games", defaultIcon: "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6901295fa9bcfaa0f5ba2c2a/cd5bb49da_image.png", circular: true },
-    { name: "Ksocial", icon: "Link2", path: "Ksocial", category: "Social", defaultIcon: "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6901295fa9bcfaa0f5ba2c2a/03dff2082_image.png", circular: true },
+    { name: "Ksocial", icon: "Link2", path: "Ksocial", category: "Social", defaultIcon: "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6901295fa9bcfaa0f5ba2c2a/7eb35a11e_image.png", circular: true },
     ];
 
   const getIconComponent = (iconName) => {
