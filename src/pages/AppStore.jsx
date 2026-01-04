65dd586519df0890f1cfd925ac9695d2cd53f1f4
Mr Zeku <mrzeku12@gmail.com>
2026-01-04 21:25:43 +0000

    File changes

--- src/pages/AppStore.jsx
+++ src/pages/AppStore.jsx
@@ -72,6 +72,7 @@ export default function AppStorePage() {
     { name: "Poki", icon: "Link2", path: "Poki", category: "Games", defaultIcon: "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6901295fa9bcfaa0f5ba2c2a/cd5bb49da_image.png", circular: true },
     { name: "Ksocial", icon: "Link2", path: "Ksocial", category: "Social", defaultIcon: "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6901295fa9bcfaa0f5ba2c2a/7eb35a11e_image.png", circular: true },
     { name: "VALORANT", icon: "Link2", path: "Valorant", category: "Games", defaultIcon: "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6901295fa9bcfaa0f5ba2c2a/0aeac6876_image.png" },
+    { name: "KasPlay", icon: "Link2", path: "KasPlay", category: "Games", defaultIcon: "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6901295fa9bcfaa0f5ba2c2a/17dc7c8d0_image.png", circular: true },
     ];
 
   const getIconComponent = (iconName) => {
